5d65ca8652bdbfca74da2b3b9a700b51156a3f22
Romier Manning <126rmanning@frhsd.com>
2024-11-20 12:51:19 -0500

    lesson 4

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
-const titles = document.getElementsByClassName('title');
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
 
-console.log(search, bookList);
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
-Array.from(titles).forEach(function(title){
-  console.log(title);
+var books = document.querySelector('#book-list li .name');
+console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
 });
